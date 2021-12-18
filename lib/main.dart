4b37ec2a1ea35f646b62cece1110de3e5c2003cc
Vaibhav <nightfurydgreat@gmail.com>
2021-12-18 23:20:18 +0530

    add textfield for input

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'pages/choose.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     initialRoute: '/',
     routes: {
       '/': (context) => const Load(),
--- lib/pages/choose.dart
+++ lib/pages/choose.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:world_time/services/worldtime.dart';
+import 'package:intl/intl.dart';
+
 
 class Chooselocation extends StatefulWidget {
   const Chooselocation({Key? key}) : super(key: key);
@@ -21,11 +23,13 @@ class _ChooselocationState extends State<Chooselocation> {
     WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
   ];
 
+
   void updatetime() async {
     WorldTime instance2 = WorldTime(
-        location: controller.text,
+
+        location: toBeginningOfSentenceCase(controller.text) ?? "",
         flag: 'usa.png',
-        url: 'Europe/${controller.text}');
+        url: '$continent/${controller.text}');
     await instance2.GetData();
     Navigator.pop(context, {
       'location': instance2.location,
@@ -35,6 +39,8 @@ class _ChooselocationState extends State<Chooselocation> {
   }
 
   TextEditingController controller = TextEditingController();
+  String value1 = " ";
+  String continent = " ";
 
   @override
   Widget build(BuildContext context) {
@@ -51,30 +57,115 @@ class _ChooselocationState extends State<Chooselocation> {
         centerTitle: true,
         elevation: 3.0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(15.0, 0, 15.0, 0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-                controller: controller,
-                decoration: const InputDecoration(
-                  hintText: "Enter City",
-                  labelText: "City",
-                  labelStyle: TextStyle(
-                    fontSize: 25.0,
-                    color: Colors.black,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(15.0, 0, 15.0, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                  controller: controller,
+                  decoration: const InputDecoration(
+                    hintText: "Enter City",
+                    labelText: "City",
+                    labelStyle: TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.black,
+                    ),
+                    border: InputBorder.none,
+                    fillColor: Colors.white,
+                    filled: true,
+                  )),
+              SizedBox(height: 15.0,),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 180.0, 0),
+                child: Container(
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text("Enter Continent:",
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.amber
+                    ),
+                    ),
                   ),
-                  border: InputBorder.none,
-                  fillColor: Colors.white,
-                  filled: true,
-                )),
-            ElevatedButton(
-                child: const Text("Show"),
-                onPressed: () {
-              updatetime();
-            }),
-          ],
+                  color: Colors.black,
+                ),
+              ),
+              Row(
+                children: [
+                  Radio(
+                    value: 'Europe', onChanged: (value) {
+                      setState(() {
+                        value1 = 'Europe';
+                        continent = "Europe";
+                      }); }, groupValue: value1,
+                  ),
+                  Text("Europe",
+                  style: TextStyle(
+                    fontSize: 30.0,
+                  ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Radio(
+                    value: 'Africa', onChanged: (value) { setState(() {
+                    value1 = 'Africa';
+                    continent = "Africa";
+                    }); }, groupValue: value1,
+                  ),
+                  Text("Africa",
+                    style: TextStyle(
+                      fontSize: 30.0,
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Radio(
+                    value: 'Asia', onChanged: (value) { setState(() {
+                    value1 = 'Asia';
+                    continent = "Asia";
+                    }); }, groupValue: value1,
+                  ),
+                  const Text("Asia",
+                    style: TextStyle(
+                      fontSize: 30.0,
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Radio(
+                    value: 'Australia', onChanged: (value) { setState(() {
+                    value1 = 'Australia';
+                    }); }, groupValue: value1,
+                  ),
+                  Text("Australia",
+                    style: TextStyle(
+                      fontSize: 30.0,
+                    ),
+                  )
+                ],
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.black,
+                ),
+                  child: const Text("Show",
+                  style: TextStyle(
+                    color: Colors.amber
+                  ),
+                  ),
+                  onPressed: () {
+                updatetime();
+              }),
+            ],
+          ),
         ),
       ),
     );
